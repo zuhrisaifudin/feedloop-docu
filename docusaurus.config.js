b46327276363d983fe1f0734516ba2e1018b1611
Zuhri Saifudin <rizalswag70@gmail.com>
2021-04-13 11:49:01 +0700

    documentation v1

---
+++ docusaurus.config.js
@@ -0,0 +1,146 @@
+
+module.exports = {
+  title: 'One Platform For Your Digitalization',
+  tagline: 'Let\'s grow through digital transformation with our platform-based solution!',
+  url: 'feedloop.io',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      hideOnScroll: true,
+      title: 'Documentation',
+      logo: {
+        alt: 'Documentation Logo Feedloop',
+        src: 'https://static.wixstatic.com/media/550988_087ff81b45b24164a119788fa88f401d~mv2.png/v1/fill/w_308,h_64,al_c,q_85/Logo%20(3).webp',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {to: 'blog', label: 'Blog', position: 'left'},
+        // Please keep GitHub link to the right for consistency. right
+        {
+          type: 'localeDropdown',
+          position: 'right',
+          dropdownItemsAfter: [
+            {
+              to: '#',
+              label: 'Help Us Translate',
+            },
+          ],
+        },
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Product',
+          items: [
+            {
+              label: 'Qore',
+              to: 'docs/',
+            },
+            {
+              label: 'AIXP',
+              to: 'docs/doc2',
+            },
+          ],
+        },
+        {
+          title: 'Solutions',
+          items: [
+            {
+              label: 'Digitalize Business Operation',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Personalized Customer Journey',
+              href: 'https://twitter.com/docusaurus',
+            },
+            {
+              label: 'Building New Digital Business',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'Legal',
+          // Please do not remove the privacy and terms, it's a legal requirement.
+          items: [
+            {
+              label: 'Privacy',
+              href: 'https://opensource.facebook.com/legal/privacy/',
+            },
+            {
+              label: 'Terms',
+              href: 'https://opensource.facebook.com/legal/terms/',
+            },
+            {
+              label: 'Data Policy',
+              href: 'https://opensource.facebook.com/legal/data-policy/',
+            },
+            {
+              label: 'Cookie Policy',
+              href: 'https://opensource.facebook.com/legal/cookie-policy/',
+            },
+          ],
+        },
+      ],
+      logo: {
+        alt: 'Feedloop Logo',
+        src: 'https://static.wixstatic.com/media/550988_801090ac6f76465489da86dd8243d2e0~mv2.png/v1/fill/w_302,h_64,al_c,q_85,usm_0.66_1.00_0.01/550988_801090ac6f76465489da86dd8243d2e0~mv2.webp',
+        href: 'https://www.feedloop.io/',
+      },
+      // Please do not remove the credits, help to publicize Docusaurus :)
+      copyright: `Copyright © ${new Date().getFullYear()} Feedloop, Inc. Built with Love.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,100 @@
+
+
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import styles from './styles.module.css';
+
+const features = [
+  {
+    title: 'Easy to Use',
+    imageUrl: 'https://static.wixstatic.com/media/550988_b7772a5a29f2424280754c8ad97fffdb~mv2.png/v1/fill/w_960,h_960,al_c,q_90,usm_0.66_1.00_0.01/Mini%20Illustration%202%20-%20Blue%201.webp',
+    description: (
+      <>
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: 'Digital Business Operation',
+    imageUrl: 'https://static.wixstatic.com/media/550988_5f736e16c48043979428b88992a0305b~mv2.png/v1/fill/w_898,h_896,al_c,q_90,usm_0.66_1.00_0.01/Mini%20Illustration%201%20-%20Blue%201.webp',
+    description: (
+      <>
+        Feedloop can help transform business operation to be more agile, allows for faster innovation and adaptation for your Company
+      </>
+    ),
+  },
+  {
+    title: 'Personalized Customer Journey',
+    imageUrl: 'https://static.wixstatic.com/media/550988_37358a56f68346dfbd14f2f1086a5238~mv2.png/v1/fill/w_786,h_786,al_c,q_90,usm_0.66_1.00_0.01/Mini%20Illustration%203%20-%20Blue%201.webp',
+    description: (
+      <>
+        Improve overall customer experience of your business to drive growth with Feedloop
+      </>
+    ),
+  },
+];
+
+function Feature({imageUrl, title, description}) {
+  const imgUrl = useBaseUrl(imageUrl);
+  return (
+    <div className={clsx('col col--4', styles.feature)}>
+      {imgUrl && (
+        <div className="text--center">
+          <img className={styles.featureImage} src={imgUrl} alt={title} />
+        </div>
+      )}
+      <h3 className="text--center">{title}</h3>
+      <p className="text--center">{description}</p>
+    </div>
+  );
+}
+
+export default function Home() {
+  const context = useDocusaurusContext();
+  const {siteConfig = {}} = context;
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <header className={clsx('hero hero--primary', styles.heroBanner)}>
+        <div className="container">
+          <h1 className="hero__title">{siteConfig.title}</h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
+          <div className={styles.buttons}>
+            <Link
+              className={clsx(
+                'button button--outline button--secondary button--lg',
+                styles.getStarted,
+              )}
+              to={useBaseUrl('docs/')}>
+              Get Started
+            </Link>
+          </div>
+        </div>
+      </header>
+      <main>
+        {features && features.length > 0 && (
+          <section className={styles.features}>
+            <div className="container">
+              <div className="row">
+                {features.map(({title, imageUrl, description}) => (
+                  <Feature
+                    key={title}
+                    title={title}
+                    imageUrl={imageUrl}
+                    description={description}
+                  />
+                ))}
+              </div>
+            </div>
+          </section>
+        )}
+      </main>
+    </Layout>
+  );
+}
